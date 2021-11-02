eb21640369646e3b11c7d52b0b9861940fe98e92
Gotthold, Benjamin <bgotthold@usgs.gov>
2021-11-02 07:55:02 -0600

    branch for Pi

--- process.sh
+++ process.sh
@@ -15,4 +15,4 @@ echo "$(date)" >> /media/pi/$STORAGE/log.txt
 pwd >> /media/pi/$STORAGE/log.txt
 which python3 >> /media/pi/$STORAGE/log.txt
 pip3 freeze >> /media/pi/$STORAGE/log.txt
-python3 /home/pi/NABat/gottbat/nabat_ml_cli.py -p /media/pi/$STORAGE -g 77474 -u a524c686-1a68-11ec-bb96-06e6624076b1  >> /media/pi/$STORAGE/log.txt 2>&1
+python3 /media/pi/$STORAGE/gottbat/nabat_ml_cli.py -p /media/pi/$STORAGE -g 77474 -u a524c686-1a68-11ec-bb96-06e6624076b1  >> /media/pi/$STORAGE/log.txt 2>&1
