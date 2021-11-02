e7c718aaefca51a99f5292f9f4df4cb57aa82670
Gotthold, Benjamin <bgotthold@usgs.gov>
2021-11-02 09:32:50 -0600

    get deployment info from settings

--- process.sh
+++ process.sh
@@ -9,4 +9,9 @@ echo "$(date)" >> /media/pi/$STORAGE/log.txt
 pwd >> /media/pi/$STORAGE/log.txt
 which python3 >> /media/pi/$STORAGE/log.txt
 pip3 freeze >> /media/pi/$STORAGE/log.txt
-python3 /media/pi/$STORAGE/gottbat/nabat_ml_cli.py -p /media/pi/$STORAGE -g 77474 -u a524c686-1a68-11ec-bb96-06e6624076b1  >> /media/pi/$STORAGE/log.txt 2>&1
+
+
+GRTS=$(cat ../settings.txt | grep GRTS | cut -d "=" -f 2)
+DETECTOR=$(cat ../settings.txt | grep DETECTOR | cut -d "=" -f 2)
+
+python3 /media/pi/$STORAGE/gottbat/nabat_ml_cli.py -p /media/pi/$STORAGE -g $GRTS -u $DETECTOR  >> /media/pi/$STORAGE/log.txt 2>&1
